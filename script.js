88ea0557e081f0dee88ba53bab7a212bc84cdf4b
aadya909 <145979679+aadya909@users.noreply.github.com>
2025-03-27 13:46:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,43 @@
-//complete this code
-class Person {}
+class Person {
+  private _name: string;
+  private _age: number;
 
-class Student extends Person {}
+  constructor(name: string, age: number) {
+    this._name = name;
+    this._age = age;
+  }
 
-class Teacher extends Person {}
+  get name(): string {
+    return this._name;
+  }
+
+  get age(): number {
+    return this._age;
+  }
+
+  set age(age: number) {
+    if (age > 0) {
+      this._age = age;
+    } else {
+      console.error("Age must be a positive number.");
+    }
+  }
+}
+
+class Student extends Person {
+  study(): void {
+    console.log(`${this.name} is studying`);
+  }
+}
+
+class Teacher extends Person {
+  teach(): void {
+    console.log(`${this.name} is teaching`);
+  }
+}
 
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
 window.Teacher = Teacher;
+
