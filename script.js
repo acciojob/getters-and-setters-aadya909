4e156810ddc79bc690906163e05231317e646f26
aadya909 <145979679+aadya909@users.noreply.github.com>
2025-03-27 13:56:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 class Person {
-  private _name: string;
-  private _age: number;
+  #name;
+  #age;
 
-  constructor(name: string, age: number) {
-    this._name = name;
-    this._age = age;
+  constructor(name, age) {
+    this.#name = name;
+    this.#age = age;
   }
 
-  get name(): string {
-    return this._name;
+  get name() {
+    return this.#name;
   }
 
-  get age(): number {
-    return this._age;
+  get age() {
+    return this.#age;
   }
 
-  set age(age: number) {
+  set age(age) {
     if (age > 0) {
-      this._age = age;  // ✅ Correctly updating age
+      this.#age = age;
     } else {
       console.error("Age must be a positive number.");
     }
@@ -25,13 +25,13 @@ class Person {
 }
 
 class Student extends Person {
-  study(): void {
+  study() {
     console.log(`${this.name} is studying`);
   }
 }
 
 class Teacher extends Person {
-  teach(): void {
+  teach() {
     console.log(`${this.name} is teaching`);
   }
 }
